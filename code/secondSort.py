c11ce7d92784b570ceeac93c8a5a02fe0dabf9a8
jessica.lee <umjessica_lee@163.com>
2018-08-08 21:39:25 +0800

    finish second sort

--- code/secondSort.py
+++ code/secondSort.py
@@ -2,13 +2,22 @@ from pyspark import SparkConf, SparkContext
 
 File='../data/temperature.txt'
 
+
+def f(x):
+    buf=""
+    for i in x[1]:
+        buf=buf+i
+        buf=buf+" ,"
+    buf=buf[0:len(buf)-1]
+    print(x[0]+" "+buf)
+
 def secondSort(file):
 
     # Create context and set environment variable
     conf=SparkConf().setAppName("Second Sort").setMaster("local")
     sc=SparkContext(conf=conf)
     sc.setLogLevel("WARN")
-
-
     text=sc.textFile(file)
-    rdd=text.map(lambda line: line.split(','))
+    rdd=text.map(lambda line: line.split(",")).map(lambda x: (x[0]+"-"+x[1], [x[3]])).sortByKey(False).reduceByKey(lambda x, y: sorted(x+y, reverse=True))
+    rdd.foreach(lambda x: f(x))
+    sc.stop()
